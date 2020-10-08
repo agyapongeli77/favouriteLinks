77b7fecc48511e17461982b43c5f6634939bc33c
agyapongeli77 <agyapongeli77@gmail.com>
2020-10-08 17:24:00 -0400

    Form updated

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,10 +4,8 @@ class Form extends Component {
     constructor(props) {
         super(props);
         
-        /*
-            TODO - set initial state for link name and URL 
-
-        */
+        //TODO - set initial state for link name and URL 
+        this.state = {name:'', URL:''};
     }
 
     handleChange = event => {
@@ -31,6 +29,11 @@ class Form extends Component {
         return(
             <form>
                 {/* TODO - Logic for returning a form element with labels and inputs for link name and URL */}
+                <label for="name">Name</label>
+                <input type="text" id="name" name="name" value={this.state.name}/>
+                <label for="url">URL</label>
+                <input type="text" id="url" name="url" value={this.state.URL}/>
+                <input type="button" value="submit" onclick={this.onFormSubmit}/>
             </form>
         )
     
