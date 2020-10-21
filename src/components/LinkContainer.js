319666edb7bdadf445259b91985fdbe40f99997c
agyapongeli77 <agyapongeli77@gmail.com>
2020-10-21 02:23:09 -0400

    removeCharacter and handleSubmit added

--- src/components/LinkContainer.js
+++ src/components/LinkContainer.js
@@ -6,18 +6,23 @@ class LinkContainer extends React.Component {
     constructor(props){
         super(props)
         /* TODO - Create state object for storing favLinks */
+        this.state = {
+            favLinks: []
+        }
     }
 
     removeCharacter = index => {
         /*
             TODO - Create logic for setting the state to filter array and remove favLink at index
         */
+       this.setState({favLinks: this.state.favLinks.splice(index)});
     }
 
     handleSubmit = favLink => {
         /*
             TODO - Create logic to setState and add new favLink to favLinks array in state
         */
+       this.setState({favLinks: this.state.favLinks.concat(favLink)});
     }
 
     render() {
@@ -27,7 +32,7 @@ class LinkContainer extends React.Component {
                 <h1>My Favorite Links</h1>
                 <p>Add a new url with a name and link to the table.</p>
                 {/*TODO - Add Table Component */}
-                
+                <Table removeLink={this.removeCharacter} linkData = {this.state.favLink}/> 
                 <br/>
                 <h3>Add New</h3>
                 {/*TODO - Add Form Component */}
